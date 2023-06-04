620e8ad3f1e975dc53a6af9e546b9d4aeaff7c4f
Earl Bit <earlbit247@gmail.com>
2023-06-04 23:39:22 +0200

    JS15

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('add').addEventListener('click', function() {
+    document.getElementById('text').textContent = 'Sample text';
+  });
+  
+  document.getElementById('delete').addEventListener('click', function() {
+    document.getElementById('text').textContent = '';
+  });
+  
